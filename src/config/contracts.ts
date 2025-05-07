5bc9d85dbcd7f6db9ceb7fc93ffd5040854a924b
warzone-stack <warzone8964@gmail.com>
2025-05-07 19:13:52 +0800

    Update .env.example and contracts configuration for vault settings
    - Updated VITE_SOL_VAULT_ADDRESS in .env.example to a new value.
    - Added VITE_SOL_TOTAL_OFFERED and VITE_SOL_ACTIVATION_POINT to .env.example for better configuration management.
    - Refactored contracts.ts to utilize new environment variables for TOTAL_OFFERED and ACTIVATION_POINT.
    - Adjusted useAlphaVaultInfo hook to use the new constants for improved clarity and maintainability.

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -1,15 +1,9 @@
 import { NATIVE_MINT } from '@solana/spl-token';
 import { Cluster, PublicKey } from '@solana/web3.js';
 
-export const VAULT_ADDRESS = new PublicKey(
-  import.meta.env.PROD
-    ? (import.meta.env.VITE_SOL_VAULT_ADDRESS as string)
-    : '64du5MMSqb9TaJH3ZZ5B9KBRTrFSifDMYFLXUkEW1RF'
-);
+export const VAULT_ADDRESS = new PublicKey(import.meta.env.VITE_SOL_VAULT_ADDRESS as string);
 
-export const CLUSTER: Cluster = import.meta.env.PROD
-  ? (import.meta.env.VITE_SOL_CLUSTER as Cluster)
-  : 'devnet';
+export const CLUSTER: Cluster = import.meta.env.VITE_SOL_CLUSTER as Cluster;
 
 export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
   'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'
@@ -18,4 +12,7 @@ export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
 export const SOL_MINT_ADDRESS = PublicKey.default.toBase58();
 export const WSOL_MINT_ADDRESS = NATIVE_MINT.toBase58();
 
-export const LOCK_DURATION = 3600 * 1;
+// https://solscan.io/tx/2RueuEtqn9VbNmpN2UQmW5whPn4PH7A8SBq77CLKjZkMsDqTieLgP2ScofpTZpoMFLaW58iDEkXGX1JphJ4jt6xb?cluster=devnet
+export const TOTAL_OFFERED = Number(import.meta.env.VITE_SOL_TOTAL_OFFERED) as number;
+
+export const ACTIVATION_POINT = Number(import.meta.env.VITE_SOL_ACTIVATION_POINT) as number;
